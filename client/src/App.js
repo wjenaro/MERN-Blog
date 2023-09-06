b4c6044c9c2cfd342099f1c15e1928143d6294d3
Jenaro Wachira <wjenaro@gmail.com>
2023-09-06 22:45:49 +0300

    start app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-<div>
-  Test
-</div>
+    <main>
+      <header>
+        <a href="" className="logo">My Blog</a>
+        <nav>
+          <a className="login" href="">Login</a>
+          <a className="logout" href="">Logout</a>
+        </nav>
+      </header>
+      <div className="entries">
+        <div className="entry">
+          <img className="image-s" src="https://images.freeimages.com/images/large-previews/d0b/chain-link-fence-in-front-of-a-1535426.jpg" alt="Fence"></img>
+         <div className="text"> 
+         <h1>5 Neat AI Startups from Y Combinator’s Summer 2023 Batch</h1>
+          <p>
+            Y Combinator works with a few hundred startups twice each year, showcasing them in a final pitch-a-thon. On Wednesday, the first half of Y Combinator’s Summer 2023 cohort told a group of investors and media members what they are working on and how much progress they have made.
+          </p>
+         </div>
+         
+        </div>
+       
+         
+        </div>
+
+    </main>
   );
 }
 
