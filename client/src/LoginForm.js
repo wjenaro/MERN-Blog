afd7e48de59504d9fef867aa5409b175fadc55eb
Jenaro <wjenaro@gmail.com>
2023-10-12 15:54:25 +0300

    Update LoginForm.js

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -21,7 +21,7 @@ function BasicLogin() {
   async function login(ev) {
     ev.preventDefault();
     try {
-      const response = await fetch('http://localhost:4000/login', {
+      const response = await fetch('https://mern-blog-api-five.vercel.app/login', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
