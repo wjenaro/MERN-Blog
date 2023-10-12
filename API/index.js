fc1f7c2755e3d407cfaa4779881f426741724623
Jenaro Wachira <wjenaro@gmail.com>
2023-10-13 00:37:14 +0300

    update index.js

--- API/index.js
+++ API/index.js
@@ -3,7 +3,9 @@ const app =express();
 //initialise Port
 const PORT =process.env.PORT || 4000;
 
-
+app.get('/', (req, res)=>{
+  res.send("Hello world");
+});
 
 //listening to server
 app.listen(PORT, ()=>{
