9404d7ede4f72dfbaaaa1082f7d04df4c183a914
Jenaro Wachira <wjenaro@gmail.com>
2023-10-13 12:57:23 +0300

    cluster0

--- API/index.js
+++ API/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Post=require("./models/Post"); 
+const Post = require('./models/Post');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Test database connection
-const DBPassWord = process.env.DB_PASSWORD || "snt5jMT0adajGAZJ";
-const DBNAME = process.env.DB_NAME || "Animals";
+const DB_PASSWORD = process.env.DB_PASSWORD || "snt5jMT0adajGAZJ";
+const DB_NAME = process.env.DB_NAME || "Animals";
 const USERNAME = process.env.USERNAME || "animalblog";
-const DB_URL = `mongodb+srv://${USERNAME}:${DBPassWord}@clustername.mongodb.net/${DBNAME}`;
+const CLUSTER_NAME = process.env.CLUSTER_NAME || "cluster0"; // Replace with your actual cluster name
+const DB_URL = `mongodb+srv://${USERNAME}:${DB_PASSWORD}@${CLUSTER_NAME}.mongodb.net/${DB_NAME}`;
 
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -22,16 +23,16 @@ mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     console.error('Error connecting to MongoDB:', err);
   });
 
-  app.get('/', async (req, res) => {
-    // Getting data from the Post collection
-    try {
-      const posts = await Post.find();  
-      res.send(posts);
-    } catch (error) {
-      console.error('Error fetching posts:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+app.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.send(posts);
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
