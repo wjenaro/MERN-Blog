cfbd910910d8903a5019ee4d69ddcf819c0a2e5c
Jenaro Wachira <wjenaro@gmail.com>
2023-10-13 12:01:35 +0300

    database test

--- API/index.js
+++ API/index.js
@@ -1,31 +1,25 @@
 const express = require('express');
-const mongoose=require('mongoose'); 
-
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-//test database connection 
-const DBPassWord=process.env.DB_PASSWORD || "snt5jMT0adajGAZJ";
-const DBNAME=process.env.DB_NAME || "Animals"; 
+// Test database connection
+const DBPassWord = process.env.DB_PASSWORD || "snt5jMT0adajGAZJ";
+const DBNAME = process.env.DB_NAME || "Animals";
+const USERNAME= process.env.USERNAME || "animalblog";
+const DB_URL = `mongodb+srv://${USERNAME}:${DBPassWord}@clustername.mongodb.net/${DBNAME}`;
 
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
-  if (err) {
-      console.error('Error connecting to MongoDB:', err);
-  } else {
-      console.log('Connected to MongoDB successfully');
-  }
-});
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
-app.get('/', (req, res)=>{
-  res.send("Hello World "); 
-})
-
-
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Internal Server Error' });
+app.get('/', (req, res) => {
+  res.send("Hello World ");
 });
 
 app.listen(PORT, () => {
