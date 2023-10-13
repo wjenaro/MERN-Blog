a2716357d2c868291325c7d3ea6ac87a0f9a0a79
Jenaro Wachira <wjenaro@gmail.com>
2023-10-13 11:33:55 +0300

    MongDB connections

--- API/index.js
+++ API/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const mongoose=require('mongoose'); 
 
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+//test database connection 
+const DBPassWord=process.env.DB_PASSWORD || "snt5jMT0adajGAZJ";
+const DBNAME=process.env.DB_NAME || "Animals"; 
+
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+  if (err) {
+      console.error('Error connecting to MongoDB:', err);
+  } else {
+      console.log('Connected to MongoDB successfully');
+  }
+});
 
 app.get('/', (req, res)=>{
   res.send("Hello World "); 
