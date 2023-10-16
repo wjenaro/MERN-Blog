e864ddb95feb045637ace11420c969681b786d69
Jenaro Wachira <wjenaro@gmail.com>
2023-10-16 09:51:57 +0300

    c

--- API/index.js
+++ API/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const Post = require("./models/Post");
-const User = require("./models/User");
+const Post = require('./models/Post');
+const User = require('./models/User');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
@@ -17,8 +17,8 @@ require('dotenv').config();
 
 // Test database connection
 const dbName = process.env.DB_NAME || 'Animals';
-const USERNAME=process.env.DB_USERNAME || "animalblog";
-const PASSWORD=process.env.DB_PASSWORD || "qYgip4zqvhp4rbUG"; 
+const USERNAME = process.env.DB_USERNAME || 'animalblog';
+const PASSWORD = process.env.DB_PASSWORD || 'qYgip4zqvhp4rbUG';
 const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.hv9kwab.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 const secret = process.env.JWT_SECRET || '70a9d0f3ef7205e387e46f7e1a5d83a87f385a0dc2d6d3b3a64256a4f0b0e9d';
 
@@ -35,10 +35,10 @@ db.on('error', (error) => {
   console.error('MongoDB connection error:', error);
 });
 
-const CLIENT = process.env.CLIENT || "https://mern-blog-client-sigma.vercel.app";
+const CLIENT = process.env.CLIENT || 'https://mern-blog-client-sigma.vercel.app';
 app.use(cors({
   origin: CLIENT,
-  methods: ["POST", "GET", "PUT"],
+  methods: ['POST', 'GET', 'PUT'],
   credentials: true,
 }));
 
@@ -53,12 +53,8 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(cookieParser());
 
-
-
-
-
- //fetch data to populate the homepage  
- app.get('/posts', async (req, res) => {
+// fetch data to populate the homepage
+app.get('/posts', async (req, res) => {
   try {
     const posts = await Post.find()
       .populate('author', 'username')
@@ -70,8 +66,8 @@ app.use(cookieParser());
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-//register
 
+// register
 app.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -93,25 +89,39 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res)=>{
-  const {username, password}=req.body;
-  const userDoc= await User.find({username});
-  const passOk=bcrypt.compareSync(password, userDoc.password);
-  if (passOk){
-    jwt.sign({username, id: userDoc._id}, secret, {}, (err, token));
-    if(err) throw err;
-    res.cookie('token', token).json({
-      id:userDoc._id,
-      username,
-    })
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.findOne({ username });
+
+    if (!userDoc) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+
+    if (!passOk) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id: userDoc._id,
+        username,
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-
-app.get("/", (req, res)=>{
-  res.send("Working");
+app.get('/', (req, res) => {
+  res.send('Working');
 });
-//profile 
+
+// profile
 app.get('/profile', (req, res) => {
   const { token } = req.cookies;
 
@@ -135,13 +145,12 @@ app.get('/profile', (req, res) => {
   });
 });
 
-
-
 const uploadMiddleware = multer({ dest: '/tmp/' });
 
 app.post('/logout', (req, res) => {
   res.clearCookie('token').json('ok');
 });
+
 app.post('/cpost', uploadMiddleware.single('imageFile'), async (req, res) => {
   try {
     if (!req.file) {
@@ -183,23 +192,15 @@ app.post('/cpost', uploadMiddleware.single('imageFile'), async (req, res) => {
   }
 });
 
-app.get('/posts', async (req, res) => {
+app.get('/post/:id', async (req, res) => {
+  const { id } = req.params;
   try {
-    const posts = await Post.find()
-      .populate('author', 'username')
-      .sort({ createdAt: -1 })
-      .limit(5);
-    res.json(posts);
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
-});
-
-app.get('/post/:id', async (req, res) => {
-  const { id } = req.params;
-  const postDoc = await Post.findById(id).populate('author', ['username']);
-  res.json(postDoc);
 });
 
 app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
@@ -266,7 +267,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Internal Server Error' });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
